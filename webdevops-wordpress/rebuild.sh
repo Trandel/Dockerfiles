78e292d9c20e4150b9c6c2c42133b99ffd0ecf0b
Tomasz Trznadel <trandel@tlen.pl>
2019-01-22 11:10:55 +0000

    Add a script to rebuild all wp images

---
+++ webdevops-wordpress/rebuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# rebuild and push all the wordpress versions
+DOCKER_REPO="webdevops-wordpress"
+DOCKER_HUB_USERNAME="trandel"
+VERSIONS=$( curl -s "https://hub.docker.com/v2/repositories/trandel/webdevops-wordpress/tags/?page_size=100" | jq -r ".results[].name" | grep -v latest )
+
+for VER in $VERSIONS; do
+  SHA1=$( curl -fsSL "https://wordpress.org/wordpress-${VER}.tar.gz.sha1" )
+  docker build --build-arg WORDPRESS_VERSION="${VER}" --build-arg WORDPRESS_SHA1="${SHA1}" -t "${DOCKER_HUB_USERNAME}/${DOCKER_REPO}:${VER}" .
+  docker push "${DOCKER_HUB_USERNAME}/${DOCKER_REPO}:${VER}";
+done
