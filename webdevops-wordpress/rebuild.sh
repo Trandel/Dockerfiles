81fdf93b78df519189f2eafcb776009ec99bab9f
Tomasz Trznadel <trznadeltomasz@gmail.com>
2022-05-10 14:56:25 +0100

    Add tag php74-latest

--- webdevops-wordpress/rebuild.sh
+++ webdevops-wordpress/rebuild.sh
@@ -9,4 +9,6 @@ for VER in $VERSIONS; do
   SHA1=$( curl -fsSL "https://wordpress.org/wordpress-${VER}.tar.gz.sha1" )
   docker build --build-arg WORDPRESS_VERSION="${VER}" --build-arg WORDPRESS_SHA1="${SHA1}" -t "${DOCKER_HUB_USERNAME}/${DOCKER_REPO}:${VER}" .
   docker push "${DOCKER_HUB_USERNAME}/${DOCKER_REPO}:${VER}";
+  docker build --build-arg WORDPRESS_VERSION="${VER}" --build-arg WORDPRESS_SHA1="${SHA1}" -t "${DOCKER_HUB_USERNAME}/${DOCKER_REPO}:php74-${VER}" -f DockerfilePHP74 .
+  docker push "${DOCKER_HUB_USERNAME}/${DOCKER_REPO}:php74-${VER}";
 done
